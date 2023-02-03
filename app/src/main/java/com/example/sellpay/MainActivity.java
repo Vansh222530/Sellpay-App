60ea62cf261d6e470eeae04ef0e02e24b3093133
Vansh2204 <Vansh2204@users.noreply.github.com>
2023-02-03 18:02:46 +0530

    Changes that are done

--- app/src/main/java/com/example/sellpay/MainActivity.java
+++ app/src/main/java/com/example/sellpay/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.sellpay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
